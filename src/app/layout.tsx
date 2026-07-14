0be9d96000a7b00b3286ae41e373b33f8afd33ac
Umar Khatri <3007uk@gmail.com>
2026-07-14 20:27:25 +0530

    chore: hide clerk development mode warning

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,7 +36,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        layout: {
+          unsafe_disableDevelopmentModeWarnings: true,
+        },
+      }}
+    >
       <html
         lang="en"
         className={`${inter.variable} ${geistMono.variable} h-full antialiased`}
